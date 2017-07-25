4dd5bfab936e035804e037c8e2d6e9872416fa7b
Crystal <crystal.r.mcgowan@gmail.com>
2017-07-24 20:21:57 -0400

    Fixed Time

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let year = `${now.format("YYYY")}`
 let midnight= `${now.startOf('day').format("s")}`
 let currentSecond = `${moment().format("s")}`
 
-console.log('It is ' + (chalk.blue.bold(`${now.format("dddd, MMMM Do YYYY, h:mm:ss a")}`) + '.'))
+console.log('It is ' + (chalk.blue.bold(`${moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}`) + '.'))
 console.log('It is the ' + (chalk.magenta.bold(`${now.format("DDDo")}`) + ' day of the year.'));
 console.log('It is ' + (chalk.green.bold(moment().diff(moment().startOf('day'), 's')) + ' seconds into the day.'));
 
